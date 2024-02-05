ae211e5eada46ad3b6479a47fa79f211f0a72360
shokhaway01 <shoxauey@gmail.com>
2024-02-05 17:22:52 +0500

    Я добавил новый файл в папке "интерполяция" чтобы показать интерполяцию в действии.

---
+++ interpolation/index.js
@@ -0,0 +1,10 @@
+let name = "ivan",
+    age = 35,
+    gender = "male";
+
+console.log("My name is -" + name + ". I'm " + age + " y.o And i'm a " + gender)
+// Это пример строки БЕЗ интерполяции
+
+
+console.log(`My name is ${name}, I'm ${age} y.o. And i'm a ${gender}`)
+// Это пример написания строки с помошью интерполяции
