59ffbe1de595aecc6d9c954064d290f7de63be24
shokhaway01 <shoxauey@gmail.com>
2024-02-05 19:21:04 +0500

    в этом каммите я хотел бы написать, что я вошел в тему JSON и начал парсить данные в области программирования. А следующис дейстием что я попытыюсь сделать, это получение данных с помошью AJAX

---
+++ json/index.js
@@ -0,0 +1,12 @@
+let options = {
+    width: 1266,
+    height: 768,
+    bgckground: "red",
+    
+    font: {
+        size: "16px",
+        color: "#fff"
+    }
+};
+
+console.log(JSON.stringify(options));
