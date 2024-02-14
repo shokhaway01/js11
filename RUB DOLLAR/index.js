be02a3cc0bde014cbe4c0db2600862f1f2edd4cc
shokhaway01 <shoxauey@gmail.com>
2024-02-14 16:09:19 +0500

    RUB\DOLLAR проект был завершен

--- RUB DOLLAR/index.js
+++ RUB DOLLAR/index.js
@@ -1,6 +1,26 @@
 let rub  = document.getElementById('rub');
 let usd  = document.getElementById('usd');
 
+usd.addEventListener('input', ()=>{
+    let request = new XMLHttpRequest();
+    request.open("GET","current.json");
+    request.setRequestHeader("Content-type","application/json; charset=utf-8");
+    request.send();
+
+
+    request.addEventListener("readystatechange", function(){
+        if(request.readyState === 4 && request.status == 200){
+            let data = JSON.parse(request.response);
+            let price = usd.value * data.usd;
+            rub.value = price.toFixed(3);
+        }
+        else{
+            rub.value = "Что-то пошло не так";
+        }
+    })
+})
+
+
 rub.addEventListener('input', ()=>{
     let request = new XMLHttpRequest();
 
@@ -21,8 +41,8 @@ rub.addEventListener('input', ()=>{
     request.addEventListener('readystatechange', () =>{
         if ( request.readyState == 4 && request.status == 200){
             let data = JSON.parse(request.response);
-
-            usd.value = rub.value / data.usd;
+            let price = rub.value / data.usd
+            usd.value = price.toFixed(3);
         }else{
             usd.value = "Что-то не так!";
         }
